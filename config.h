919658d9a9eb2eb954c8b3254377b805a93aec4b
Justin Breese <particle_m@hotmail.com>
2021-11-10 16:34:05 +0000

    Add files via upload

---
+++ config.h
@@ -0,0 +1,24 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
+
+/* Name of package */
+#define PACKAGE "stellarlegacy"
+
+/* Version number of package */
+#define VERSION "0.2"
+
+/* C++ compiler supports template repository */
+#define HAVE_TEMPLATE_REPOSITORY 1
+
+/* define if the compiler implements namespaces */
+#define HAVE_NAMESPACES 
+
+/* define if the compiler supports ISO C++ standard library */
+#define HAVE_STD 
+
+/* define if the compiler supports sstream */
+#define HAVE_SSTREAM 
+
+/* define if libstdc++v3 was found */
+/* #undef HAVE_LIBSTDCXX */
+
